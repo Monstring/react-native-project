5ee26f430a691e86030bd4ef8a225a152c6979be
Monstring <monstring1@hotmail.com>
2019-06-15 13:41:44 +0300

    Created a sign-up page

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 
 export default function App() {
-  return <LoginPage />;
+  return <SignupPage />;
 }
---
+++ components/SignupPage.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+
+export default class SignupPage extends Component {
+  state = {
+    Username: undefined,
+    Password: undefined
+  };
+
+  StoreUsername = Username => {
+    this.setState({ Username: Username });
+  };
+  StorePassword = Password => {
+    this.setState({ Password: Password });
+  };
+
+  render() {
+    return (
+      <View style={{ paddingTop: 50 }}>
+        <TextInput placeholder="Username" onChangeText={this.StoreUsername} />
+        <TextInput placeholder="Password" onChangeText={this.StorePassword} />
+        <TextInput placeholder="Confirm password" />
+        <Button
+          style={{ paddingTop: 10 }}
+          title="Sign up"
+          disabled={false}
+          onPress={() => console.log(JSON.stringify(this.state))}
+        />
+      </View>
+    );
+  }
+}
