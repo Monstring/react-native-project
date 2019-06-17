eaacc7e03bf9e4f43d0008f15a29054f91e355a3
Monstring <monstring1@hotmail.com>
2019-06-17 11:10:42 +0300

    Styling added to the SignUp page

--- components/SignupPage.js
+++ components/SignupPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, CheckBox, TextInput, Button } from "react-native";
+import { View, StyleSheet, TextInput, Button, } from "react-native";
 
 export default class SignupPage extends Component {
   state = {
@@ -16,17 +16,35 @@ export default class SignupPage extends Component {
 
   render() {
     return (
-      <View style={{ paddingTop: 50 }}>
+      <View style={styles.container}>
+        <View style={styles.half1}>
+
+        </View>
+        <View style={styles.half2}>
         <TextInput placeholder="Username" onChangeText={this.StoreUsername} />
         <TextInput placeholder="Password" onChangeText={this.StorePassword} secureTextEntry={true} />
         <TextInput placeholder="Confirm password" />
         <Button
-          style={{ paddingTop: 10 }}
           title="Sign up"
           disabled={false}
           onPress={() => console.log(JSON.stringify(this.state))}
         />
+        </View>
       </View>
     );
   }
+
+  
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  half1:{
+    flex: 2,
+  },
+  half2: {
+    flex: 4,
+    alignItems: 'center'
+  }
+})
